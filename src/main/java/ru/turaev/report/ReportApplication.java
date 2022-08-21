ea40c31b861fb97dbc476759412a16e0557ed270
Turaev <denis.turaev@bk.ru>
2022-08-21 11:40:16 +0300

    Initial commit

---
+++ src/main/java/ru/turaev/report/ReportApplication.java
@@ -0,0 +1,13 @@
+package ru.turaev.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ReportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReportApplication.class, args);
+    }
+}
